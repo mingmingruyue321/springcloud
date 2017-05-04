cdbe3c6c013b85ac2fb72a17376d0c61234ec2d2
mmry <1105255090@qq.com>
2017-05-04 09:14:58 +0800

    metadata

---
+++ config/src/main/java/mmry/config/App.java
@@ -0,0 +1,13 @@
+package mmry.config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
